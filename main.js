c4a047d33167c8e8e1c22af7f410789fcb2c6b6f
Daniel <mikyjo2012@gmail.com>
2022-07-18 16:24:49 -0500

    add basic functions and html

---
+++ main.js
@@ -0,0 +1,18 @@
+function add (x, y){
+    return x + y;
+}
+function subtract (x, y) {
+    return x - y;
+}
+function multiply (x, y) {
+    return x * y;
+}
+function divide (x, y) {
+    return x / y;
+}
+function operate(operator, x, y) {
+    if(operator == '+') return add(x, y);
+    else if(operator == '-') return subtract(x, y);
+    else if(operator == '*') return multiply(x, y);
+    else if(operator == '/') return divide(x, y);
+}

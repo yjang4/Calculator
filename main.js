b1d73a61bad57f13be935f458077c487efaefe7d
Daniel <mikyjo2012@gmail.com>
2022-07-18 19:58:38 -0500

    implement operator buttons

--- main.js
+++ main.js
@@ -15,22 +15,49 @@ function operate(operator, x, y) {
     else if(operator == '-') return subtract(x, y);
     else if(operator == '*') return multiply(x, y);
     else if(operator == '/') return divide(x, y);
+    else return 0;
 }
-let btn = document.getElementById('add-btn');
 let numStore = '';
 let operatorStore = '';
-
+let result;
 for(i = 0; i <= 9; i ++) {
     let btn = document.getElementById(`num${i}-btn`);
     let display = document.getElementById("display");
     btn.addEventListener("click", function() {
-        if(operatorStore == '') {
+        if(numStore != '0') {
             display.textContent += btn.textContent;
+        }
+        else {
+            display.textContent = btn.textContent;
         }
         
     });
 }
 
-btn.addEventListener("click", function() {
-    numStore = document.getElementById("display").textContent;
+let addBtn = document.getElementById('add-btn');
+addBtn.addEventListener("click", function() {
+    if(numStore != '' && operatorStore != '') {
+        result = operate(operatorStore, numStore, display.textContent);
+    }
+    else if(numStore == '') {
+        numStore = display.textContent;
+        display.textContent = '0';
+    }
+    operatorStore = '+';
+});
+let subtractBtn = document.getElementById('subtract-btn');
+subtractBtn.addEventListener("click", function() {
+    operatorStore = '-';
+});
+let multiplyBtn = document.getElementById('multiply-btn');
+multiplyBtn.addEventListener("click", function() {
+    operatorStore = '*';
+});
+let divideBtn = document.getElementById('divide-btn');
+divideBtn.addEventListener("click", function() {
+    operatorStore = '/';
+});
+let equalBtn = document.getElementById('equal-btn');
+equalBtn.addEventListener("click", function() {
+    display.textContent += `${result}`;
 });
